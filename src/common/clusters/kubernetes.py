9a8ad5896947b7e19caa370016f7510dbfa9cdec
Marc Ende <me@e-beyond.de>
2024-12-05 15:06:15 +0100

    Mod

--- src/common/clusters/kubernetes.py
+++ src/common/clusters/kubernetes.py
@@ -15,3 +15,4 @@ class KubernetesCluster(Cluster):
         opts=None,
     ):
         super().__init__("Kubernetes", name, node_count, master_nodes, cluster_network, firewall_ip, server_type, opts)
+        pass
