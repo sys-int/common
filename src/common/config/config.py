6a0e8fb13a00a0370071b2074a958bad2be05525
Marc Ende <me@e-beyond.de>
2024-12-06 13:08:58 +0100

    Mod

---
+++ src/common/config/config.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+from pulumi import Output
+from pulumi_hcloud import Network
+
+
+class Config:
+    current_network: Network
+    networks: dict[str, Any]
+    ssh_keys: list[str]
+    firewall: Output[str]
+
+
+class ClusterConfig(Config):
+    cluster_name: str = ""
+    node_count: int = 0
+    master_nodes: int = 0
+
+    def getNodeCount(self):
+        return self.node_count + self.master_count
+
+
+class KubernetesConfig(ClusterConfig): ...
